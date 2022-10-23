c4a677634adfc2416fa19d34c7efca0b891ca5a5
teohbenji <teohbenjamin1@gmail.com>
2022-10-23 16:17:32 +0800

    Initial commit

---
+++ pset_1.py
@@ -0,0 +1,121 @@
+#declare dictionary, key-value pair
+# key: qn number
+# value: function name without string
+#https://www.geeksforgeeks.org/python-store-function-as-dictionary-value/
+
+def pset_101():
+    weight = float(input('What is your weight in kg?'))
+    height = float(input('What is your height in m?'))
+    bmi = weight/(height*height)
+    print('Your BMI is', round(bmi,2))
+    if (bmi < 18 or bmi > 25):
+        print("Good nutrition is important")
+    else:
+        print("Congratulations, Don't forget to exercise regularly")
+
+def test_101():
+    print("This is a test for pset 101")
+    print("Demo:")
+    print("Test case 1: Success")
+    print("Test case 2: Failure")
+    print("Test case 3: Success")
+
+# def pset_102():
+
+
+# def pset_103():
+
+
+# def pset_104():
+
+
+# def pset_105():
+
+
+# def pset_106():
+
+
+# def pset_107():
+
+
+# def pset_108():
+
+
+# def pset_109():
+
+
+# def pset_110():
+
+
+# def pset_111():
+
+
+# def pset_112():
+
+
+# def pset_113():
+
+#Save functions as dict values
+#https://stackoverflow.com/questions/9205081/is-there-a-way-to-store-a-function-in-a-list-or-dictionary-so-that-when-the-inde
+pset_dict = {
+    "101": pset_101
+    # "102": pset_102,
+    # "103": pset_103,
+    # "104": pset_104,
+    # "105": pset_105,
+    # "106": pset_106,
+    # "107": pset_107,
+    # "108": pset_108,
+    # "109": pset_109,
+    # "110": pset_110,
+    # "111": pset_111,
+    # "112": pset_112,
+    # "113": pset_113,
+}
+
+test_dict = {
+    "101": test_101
+    # "102": test_102,
+    # "103": test_103,
+    # "104": test_104,
+    # "105": test_105,
+    # "106": test_106,
+    # "107": test_107,
+    # "108": test_108,
+    # "109": test_109,
+    # "110": test_110,
+    # "111": test_111,
+    # "112": test_112,
+    # "113": test_113,
+}
+
+#User can choose the specific function to run
+#https://stackoverflow.com/questions/9205081/is-there-a-way-to-store-a-function-in-a-list-or-dictionary-so-that-when-the-inde
+def run_pset():
+    num = input("Which pset do you want to run?")
+    if (True):
+        print("You have selected pset" + num)
+        pset_dict[num]()
+    else:
+        print("Invalid pset number entered.")
+
+#User can choose the specific function to test
+def test_pset():
+    num = input("Which pset do you want to test?")
+    if (True):
+        print("You have selected pset" + num,"\n"+"Pset",num,"Test results:")
+        pset_dict[num]()
+    else:
+        print("Invalid pset number entered.")
+
+def run_program():
+    choice = input("Enter run to manually test a pset.\nEnter test to automatically test a pset.\nSelection:")
+    if (choice == "run"):
+        run_pset()
+    elif (choice =="test"):
+        test_pset()
+    else: 
+        print("Invalid input. Please try again.")
+        run_program()    
+
+run_program()
