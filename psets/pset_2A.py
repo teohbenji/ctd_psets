f27604c7242fc35eb0adb52301c01308d56b6b97
teohbenji <teohbenjamin1@gmail.com>
2022-11-06 01:28:36 +0800

    Completed pset2A. Setup test cases for pset2A using unittest module

--- psets/pset_2A.py
+++ psets/pset_2A.py
@@ -1,24 +1,77 @@
+import math
+
+#Calculates bmi given a weight(kg) and height (cm)
 def calculate_bmi(weight, height):
     height = height / 100
 
     bmi = round(weight /(height * height), 1)
     
     return bmi
-    
+
+#Calculates position and velocity using the SUVAT formulas    
 def position_velocity(vo, t):
-    pass
+    g = 9.81
 
+    position = round(vo * t - 0.5 * g * pow(t, 2), 3)
+    velocity = round(vo - g * t, 3)
+
+    return position, velocity
+
+#Calculates displacement using decay formula
 def decay(a, t):
-    pass
+    displacement = math.exp(-a * t) * math.cos(a * t)
 
+    return displacement
+
+#Returns description of bmi based on bmi value
 def describe_bmi(bmi):
-    pass
+    if(bmi < 18.5):
+        return "nutritional deficiency"
+    elif(bmi >= 18.5 and bmi < 23):
+        return "low risk"
+    elif(bmi >= 23 and bmi < 27.5):
+        return "moderate risk"
+    else:
+        return "high risk" 
 
+#Returns true if a number is positive and even
 def is_positive_even(n):
-    pass
+    return n % 2 == 0 and n > 0
 
+#Returns a letter grade based on mark received 
 def letter_grade(mark):
-    pass
+    if (mark >= 90 and mark <= 100):
+        return "A"
+    elif (mark >= 80 and mark <= 89):
+        return "B"
+    elif (mark >= 70 and mark <= 79):
+        return "C"
+    elif (mark >= 60 and mark <= 69):
+        return "D"
+    elif (mark >= 0 and mark <= 59):
+        return "E"        
+    else:
+        return None
 
+#Returns None if any negative value is input, or if u and v are larger than s
+#Else calculates and returns largest area
 def largest_area(s, u, v):
-    pass
+    if (s < 0 or u < 0 or v < 0):
+        return None
+    elif (s < u or s < v):
+        return None
+    
+    #Length is the greater between v, and the other length s - v 
+    if (v >= s - v):
+        length = v
+    else:
+        length = s - v
+    
+    #Breadth is the greater between u, and the other breadth s - u
+    if (u >= s - u):
+        breadth = u
+    else: 
+        breadth = s - u
+    
+    return length * breadth
+        
--- tests/test_pset_2A.py
+++ tests/test_pset_2A.py
@@ -1,8 +1,55 @@
+# https://www.youtube.com/watch?v=6tNS--WetLI
 import unittest
+from psets import pset_2A
 
-if __name__ == "__main__":
+#class that inherits from unittest.TestCase
+
+class TestPset2A(unittest.TestCase):
+    def test_calculate_bmi(self):
+        self.assertEqual(pset_2A.calculate_bmi(85.12,185.55), 24.7, "Should be 24.7")
+        self.assertEqual(pset_2A.calculate_bmi(147.34,216.688), 31.4, "Should be 31.4")
+        self.assertEqual(pset_2A.calculate_bmi(67.56,169.1), 23.6, "Should be 23.6")
+
+    def test_position_velocity(self):
+        self.assertEqual(pset_2A.position_velocity(5.0, 0), (0.0, 5.0), "Should be (0.0, 5.0)")
+        self.assertEqual(pset_2A.position_velocity(10.0, 1), (5.095, 0.19), "Should be (5.095, 0.19)")
+        self.assertEqual(pset_2A.position_velocity(5.886, 0.3), (1.324, 2.943), "Should be (1.324, 2.943)")
+
+    def test_decay(self): 
+        self.assertEqual(pset_2A.decay(2, 0), 1.0, "Should be 1.0")
+        self.assertEqual(pset_2A.decay(2, 0.5), 0.19876611034641298, "Should be 0.19876611034641298")
+
+    def test_describe_bmi(self): 
+        self.assertEqual(pset_2A.describe_bmi(18), "nutritional deficiency", 'Should be "nutritional deficiency"')
+        self.assertEqual(pset_2A.describe_bmi(18.5), "low risk", 'Should be "low risk"')
+        self.assertEqual(pset_2A.describe_bmi(23), "moderate risk", 'Should be "moderate risk"')
+        self.assertEqual(pset_2A.describe_bmi(27.5), "high risk", 'Should be "high risk"')
+
+    def test_describe_bmi(self): 
+        self.assertFalse(pset_2A.is_positive_even(-1), "Should be False")
+        self.assertTrue(pset_2A.is_positive_even(32), "Should be True")
+        self.assertFalse(pset_2A.is_positive_even(1), "Should be False")
+
+    def test_letter_grade(self): 
+        self.assertEqual(pset_2A.letter_grade(90), "A", "Should be A")
+        self.assertEqual(pset_2A.letter_grade(80), "B", "Should be B")
+        self.assertEqual(pset_2A.letter_grade(70), "C", "Should be C")
+        self.assertEqual(pset_2A.letter_grade(60), "D", "Should be D")
+        self.assertEqual(pset_2A.letter_grade(59), "E", "Should be E")
+        self.assertIsNone(pset_2A.letter_grade(420), "Should be None")
+        self.assertIsNone(pset_2A.letter_grade(-21), "Should be None")
+
+
+    def test_largest_area(self):
+        self.assertIsNone(pset_2A.largest_area(-1, 2, 3), "Should be None")
+        self.assertIsNone(pset_2A.largest_area(1, -2, 3), "Should be None")
+        self.assertIsNone(pset_2A.largest_area(1, 2, -3), "Should be None")
+        self.assertIsNone(pset_2A.largest_area(1, 2, 3), "Should be None")
+        self.assertEqual(pset_2A.largest_area(10, 3, 4), 42, "Should be 42")
+        self.assertEqual(pset_2A.largest_area(10, 3, 2), 56, "Should be 56")
+        self.assertEqual(pset_2A.largest_area(10, 2, 6), 48, "Should be 48")
+        self.assertEqual(pset_2A.largest_area(0, 0, 0), 0, "Should be 0")
+        self.assertEqual(pset_2A.largest_area(10, 0, 0), 100, "Should be 100")
+
+if __name__ == '__main__':
     unittest.main()
-
-# print(calculate_bmi(2.5, 50))
-# print(calculate_bmi(50, 150))
-# print(calculate_bmi(43.5, 142.3))
