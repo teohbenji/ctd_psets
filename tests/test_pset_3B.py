ecad25dd1bc16459d6b6b6675d83d6ca808cc56f
teohbenji <teohbenjamin1@gmail.com>
2022-11-06 18:46:19 +0800

    Initialised files for psets 3B and 4

---
+++ tests/test_pset_3B.py
@@ -0,0 +1,8 @@
+import unittest
+from psets import pset_3B
+
+class TestPset3B(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_pset_4.py
@@ -0,0 +1,8 @@
+import unittest
+from psets import pset_4
+
+class TestPset4(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
