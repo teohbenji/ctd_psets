4bee8bc20e4818536b31c042cedaada14d662ead
teohbenji <teohbenjamin1@gmail.com>
2022-11-09 22:37:00 +0800

    Completed pset 4

--- psets/pset_4.py
+++ psets/pset_4.py
@@ -22,12 +22,12 @@ def bmi_information(weight, height):
 
 #Using loop, append reversed list with element from the end of s
 def reverse(s):
-    reversed_list = []
+    reversed_string = ""
     #Access s from the right, using negative indexes
     for i in range(0, len(s)):
-        reversed_list.append(s[-(i + 1)]) 
+        reversed_string = reversed_string + str(s[-(i + 1)]) 
 
-    return ''.join(reversed_list)
+    return reversed_string
 
 #Same function, but using list slicing instead
 def reverse_other_method(s):
@@ -35,7 +35,107 @@ def reverse_other_method(s):
 
 def is_palindrome(s):
     reversed_string = s[::-1]
-    if (s == reversed_string):
+    if(s == reversed_string):
         return True
     else:
         return False
+
+#Compares ending with same length of characters from end of word
+def match(ending, word):
+    return ending == word[-len(ending):]
+
+#Removes non-alphanumeric characters, keeps whitespaces
+def clean_string(s):
+    cleaned_string = ""
+
+    for char in s:
+        if(char == ' ' or char.isalnum()):
+            cleaned_string = cleaned_string + str(char)
+
+    return cleaned_string
+
+def digits_in_string(s):
+    digit_count = 0
+
+    for char in s:
+        if(char.isnumeric()):
+            digit_count += 1
+
+    return digit_count
+
+#Check if password has at least 8 characters
+#Only alphanumeric
+#Need at least 2 numbers
+def check_password(pwd):
+    is_password_eight = len(pwd) < 8  
+    is_password_2d = digits_in_string(pwd) < 2
+
+    if(is_password_eight or is_password_2d):
+        return False
+
+    for char in pwd:
+        is_password_alnum = char.isalnum()
+
+        if not(is_password_alnum):
+            return False
+    
+    return True
+
+def longest_common_prefix(s1, s2):
+    prefix_string = ""
+
+    if(len(s1) < len(s2)):
+        shorter_string = s1
+    else:
+        shorter_string = s2
+
+    #Get indexes where both the characters in both strings have the same value
+    for i in range(len(shorter_string)):
+        if(s1[i] == s2[i]):
+            prefix_string = shorter_string[:i+1]
+        else: 
+            break
+
+    return prefix_string
+
+def binary_to_decimal(s):
+    sum = 0 
+
+    for i in range(len(s)):
+        sum += int(s[-(i + 1)]) * (2 ** i)
+
+    return sum
+
+def uncompressed(s):
+    uncompressed_str = ""
+
+    for char in s:
+        is_numeric = char.isnumeric()
+        is_alphabet = char.isalpha()
+
+        if(is_numeric):
+            digit = char
+
+        if(is_alphabet):
+            for i in range(int(digit)):
+                uncompressed_str = uncompressed_str + char
+            
+    return uncompressed_str
+
+def get_base_counts(dna):
+    #A_count, C_count, G_count, T_count
+    letter_counts = [0, 0, 0, 0]
+
+    for char in dna:
+        if(char == 'A'):
+            letter_counts[0] = letter_counts[0] + 1
+        elif(char == 'C'):
+            letter_counts[1] = letter_counts[1] + 1
+        elif(char == 'G'):
+            letter_counts[2] = letter_counts[2] + 1
+        elif(char == 'T'):
+            letter_counts[3] = letter_counts[3] + 1
+        else: 
+            return []
+    
+    return letter_counts
--- tests/test_pset_4.py
+++ tests/test_pset_4.py
@@ -19,5 +19,46 @@ class TestPset4(unittest.TestCase):
         self.assertTrue(pset_4.is_palindrome("racecar"), "Should be True as equal")
         self.assertTrue(pset_4.is_palindrome("kayak"), "Should be False as equal")
 
+    def test_match(self):
+        self.assertFalse(pset_4.match("mob", "mobile"), "mob and ile dont match")
+        self.assertFalse(pset_4.match("occe", "soccer"), "occe and ccer dont match")
+        self.assertTrue(pset_4.match("bile", "mobile"), "Should be True as equal")
+        self.assertTrue(pset_4.match("ccer", "soccer"), "Should be True as equal")
+
+    def test_clean_string(self):
+        self.assertEqual(pset_4.clean_string("It's over Anakin, I have the high ground."), "Its over Anakin I have the high ground", 'Should be "Its over Anakin I have the high ground"')
+        self.assertEqual(pset_4.clean_string("Ankara Messi, ankara Messi, ankara Messi!"), "Ankara Messi ankara Messi ankara Messi", 'Should be "Ankara Messi ankara Messi ankara Messi"')
+
+    def test_digits_in_string(self):
+        self.assertEqual(pset_4.digits_in_string("Hello"), 0, "String has 0 digits")
+        self.assertEqual(pset_4.digits_in_string("a1b2c3"), 3, "String has 3 digits")
+        self.assertEqual(pset_4.digits_in_string("2134132"), 7, "String has 7 digits")
+
+    def test_check_password(self):
+        self.assertFalse(pset_4.check_password("Hello"), "False because pwd contains less than 8 characters")
+        self.assertFalse(pset_4.check_password("1943h32%!"), "False because pwd contains non-alphanumeric characters")
+        self.assertFalse(pset_4.check_password("oqfjojw1"), "False because pwd doesn't contain 2 or more digits")
+        self.assertTrue(pset_4.check_password("I1aslf1980ad"), "Valid because pwd meets all requirements")
+
+    def test_longest_common_prefix(self):
+        self.assertEqual(pset_4.longest_common_prefix("Man", "Manga"), "Man", "Man is the common prefix")
+        self.assertEqual(pset_4.longest_common_prefix("Sing", "Singer"), "Sing", "Sing is the common prefix")
+        self.assertEqual(pset_4.longest_common_prefix("Medic", "Imedi"), "", "No common prefix")
+
+    def test_binary_to_decimal(self):
+        self.assertEqual(pset_4.binary_to_decimal("010"), 2 , "Should be 2")
+        self.assertEqual(pset_4.binary_to_decimal("01111"), 15, "Should be 15")
+        self.assertEqual(pset_4.binary_to_decimal("1011111"), 95, "Should be 95")
+
+    def test_uncompresssed(self):
+        self.assertEqual(pset_4.uncompressed("1S1I6U"), "SIUUUUUU", 'Should be "SIUUUU"')
+        self.assertEqual(pset_4.uncompressed("0a2b0c"), "bb", 'Should be "bb"')
+        self.assertEqual(pset_4.uncompressed("3x1Y2z"), "xxxYzz", 'Should be "xxxYzz"')
+
+    def test_get_base_counts(self):
+        self.assertEqual(pset_4.get_base_counts("AAACCCG"), [3, 3, 1, 0], "Should be [3, 3, 1, 0]")
+        self.assertEqual(pset_4.get_base_counts("ACCCCCGGGGGTT"), [1, 5, 5, 2], "Should be [1, 5, 5, 2]")
+        self.assertEqual(pset_4.get_base_counts("AAACCCa"), [], "Should be []")
+
 if __name__ == '__main__':
     unittest.main()
