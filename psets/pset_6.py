f99ee3c06cc540f6977cbe0a2dd4720e3bd51a4c
teohbenji <teohbenjamin1@gmail.com>
2022-11-15 16:35:14 +0800

    Completed problems 6.03 and 6.04 in pset6

--- psets/pset_6.py
+++ psets/pset_6.py
@@ -22,3 +22,45 @@ def has_list(ls):
             return True
     
     return False
+
+def max_list(inlist):
+    out = []
+
+    # If first element is not list, do not iterate
+    isList = isinstance(inlist[0], list)
+    
+    if(not isList):
+        out.append(max(inlist))
+    else:
+        for nested_list in inlist:
+            out.append(max(nested_list))
+
+    return out
+
+def find_average_of_list(ls):
+    return sum(ls) / len(ls)
+
+def find_average_of_lists(ls):
+    averages = []
+    element_count = 0
+    element_sum = 0
+
+    # If first element is not list, do not iterate
+    isList = isinstance(ls[0], list)
+
+    if(not isList):
+        rounded_average = round(find_average_of_list(ls), 2)
+        averages.append(rounded_average)
+
+        return averages, rounded_average
+    
+    for nested_list in ls:
+        averages.append(round(find_average_of_list(nested_list), 2))
+        element_sum = element_sum + sum(nested_list) 
+        element_count = element_count + len(nested_list)
+
+    average = round(element_sum / element_count, 2)
+
+    return averages, average
+
+    
--- tests/test_pset_6.py
+++ tests/test_pset_6.py
@@ -13,3 +13,13 @@ class TestPset6(unittest.TestCase):
         self.assertTrue(pset_6.has_list([[1, 2], 2, "dog"]), "True. Contains list [1, 2]")
         self.assertFalse(pset_6.has_list([1, 2, "dog"]), "False. No list.")
         self.assertFalse(pset_6.has_list([1, 2, 3]), "False. No list.")
+
+    def test_max_list(self):
+        self.assertEqual(pset_6.max_list([100, 1, 2]), [100], "Should be [100]")
+        self.assertEqual(pset_6.max_list([[1, 10, 2], [8, 7], [3, 4, 2, 1]]), [10, 8, 4], "Should be [10, 8, 4]")
+        self.assertEqual(pset_6.max_list([[1, 2, 3], [8, 7], [9, 4, 2, 1]]), [3, 8, 9], "Should be [3, 8, 9]")
+
+    def test_find_average_of_lists(self):
+        self.assertEqual(pset_6.find_average_of_lists([[1, 2], [3, 2, 5], [20, 1, 3]]), ([1.5, 3.33, 8], 4.62), "Rounded averages: [1.5, 3.33, 8], Average is 4.62 (rounded down from 4.625)")    
+        self.assertEqual(pset_6.find_average_of_lists([[6, 5], [3, 2, 6], [0, 1, 2]]), ([5.5, 3.67, 1], 3.12), "Rounded averages: [5.5, 3.67, 1], Average is 3.12 (rounded down from 3.125)")    
+        self.assertEqual(pset_6.find_average_of_lists([-6, 5]), ([-0.5], -0.5), "Rounded averages: [-5.5], Average is -5.5")    
